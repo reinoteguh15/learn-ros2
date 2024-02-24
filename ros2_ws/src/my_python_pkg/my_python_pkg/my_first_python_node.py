139bd7046a3b482f70f58267004fcb90307b876c
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2024-02-24 23:20:29 +0700

    Improve the Python node using OOP

--- ros2_ws/src/my_python_pkg/my_python_pkg/my_first_python_node.py
+++ ros2_ws/src/my_python_pkg/my_python_pkg/my_first_python_node.py
@@ -2,10 +2,21 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2! py_test node is started...")
+        self.get_logger().info("Counter is starting...")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info(f"Hello {str(self.counter_)}")
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")
-    node.get_logger().info("Hello ROS2!")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 

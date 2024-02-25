1196c68945dbd2a9fe88bb2313a10d2dd9c61487
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2024-02-25 23:21:01 +0700

    Improve the C++ node using OOP

--- ros2_ws/src/my_cpp_pkg/src/my_first_cpp_node.cpp
+++ ros2_ws/src/my_cpp_pkg/src/my_first_cpp_node.cpp
@@ -1,10 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+public:
+    MyNode() : Node("cpp_test"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello CPP node!");
+        
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds(1),
+            std::bind(&MyNode::timerCallback, this)
+        );
+    }
+
+private:
+    int counter_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    v
+    oid timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+};
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello CPP node!");
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;

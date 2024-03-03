d2a041b2acf77a7474fc87127d14472aeb15e607
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2024-03-04 00:29:29 +0700

    [Exercise] Activity 002 - Publisher and Subscriber

--- ros2_ws/src/my_python_pkg/setup.py
+++ ros2_ws/src/my_python_pkg/setup.py
@@ -22,7 +22,9 @@ setup(
         'console_scripts': [
             "py_node = my_python_pkg.my_first_python_node:main",
             "robot_news_station = my_python_pkg.robot_news_station:main",
-            "smartphone = my_python_pkg.smartphone:main"
+            "smartphone = my_python_pkg.smartphone:main",
+            "number_publisher = my_python_pkg.number_publisher:main",
+            "number_counter = my_python_pkg.number_counter:main"
         ],
     },
 )

bef5171aa8b9c67bd673481e9695ad19b49bcf6c
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2024-07-11 23:39:53 +0700

    Create a Python Service Client - No OOP

--- ros2_ws/src/my_python_pkg/setup.py
+++ ros2_ws/src/my_python_pkg/setup.py
@@ -25,7 +25,8 @@ setup(
             "smartphone = my_python_pkg.smartphone:main",
             "number_publisher = my_python_pkg.number_publisher:main",
             "number_counter = my_python_pkg.number_counter:main",
-            "add_two_ints_server = my_python_pkg.add_two_ints_server:main"
+            "add_two_ints_server = my_python_pkg.add_two_ints_server:main",
+            "add_two_ints_client_no_oop = my_python_pkg.add_two_ints_client_no_oop:main"
         ],
     },
 )
